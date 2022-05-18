ef3ec271aba604f642ded40d792cea17f053d492
Shenal99 <shenal3332018@gmail.com>
2022-05-18 11:08:37 +0530

    Server

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js'
+import cors from 'cors'
+import path from 'path'
+
+dotenv.config()
+
+//connect database
+connectDB()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(cors())
+app.use(express.json())
+
+
+
+//create port
+const PORT = process.env.PORT || 6500
+
+app.listen(
+  PORT,
+  console.log(
+    `server running in ${process.env.NODE_ENV} port ${PORT}`
+  )
+)
